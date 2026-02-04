29d4615333edd8d42c03a7c954fa5873005cdc3a
Dave Syer <david.syer@broadcom.com>
2026-02-04 08:26:45 +0000

    Revert to snapshots

--- samples/grpc-client/src/test/java/org/springframework/grpc/sample/GrpcClientApplicationTests.java
+++ samples/grpc-client/src/test/java/org/springframework/grpc/sample/GrpcClientApplicationTests.java
@@ -26,7 +26,7 @@ public class GrpcClientApplicationTests {
 	}
 
 	static boolean serverJarAvailable() {
-		return new File("../grpc-server/target/grpc-server-sample-1.0.2.jar").exists();
+		return new File("../grpc-server/target/grpc-server-sample-1.0.3-SNAPSHOT.jar").exists();
 	}
 
 	@Test
@@ -42,7 +42,7 @@ public class GrpcClientApplicationTests {
 		static CommonsExecWebServerFactoryBean grpcServer() {
 			return CommonsExecWebServerFactoryBean.builder()
 				.classpath(classpath -> classpath
-					.entries(new MavenClasspathEntry("org.springframework.grpc:grpc-server-sample:1.0.2"))
+					.entries(new MavenClasspathEntry("org.springframework.grpc:grpc-server-sample:1.0.3-SNAPSHOT"))
 					.entries(MavenClasspathEntry.springBootDependency("spring-boot-web-server"))
 					.files("target/test-classes"));
 		}
